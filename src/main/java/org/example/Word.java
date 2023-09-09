82485e8c0f424cc28aa582a9cfeb94647c3ef3ef
haeun-sung <hahashe@handong.ac.kr>
2023-09-09 22:32:31 +0900

    initial commit

--- src/main/java/org/example/Word.java
+++ src/main/java/org/example/Word.java
@@ -46,7 +46,7 @@ public class Word {
     }
     public String toString(){
         String slevel = "";
-        for(int i=0 ; i<level; i++) slevel +="+";
+        for(int i=0 ; i<level; i++) slevel +="*";
         String str = String.format("%-3s", slevel) + String.format("%15s",word)+" "+ meaning;
         return str;
     }
